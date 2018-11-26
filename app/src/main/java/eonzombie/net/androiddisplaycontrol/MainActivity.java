e524f50e2355c5622e73565392591283b01c2a41
BuildTools <unconfigured@null.spigotmc.org>
2018-11-26 23:41:45 +0100

    AndroidDisplayControl 1.0
    Author: Leopold Hauptman aka EonZombie
    
    Allows you to controll 2 x 4 led display.
    
    How it works:
    1. Click on Bluetooth button to get to device selector menu.
    Already paired devices will be displayed. Click on scan button to find
    new devices (but I recommand pairing them by using builtin bluetooth
    menu)
    
    2.Upon selecting a device, it's Bluetooth MAC address gets stored for
    further use and you get redirected back to main menu.
    
    3. Fill the text boxes with character (or let them be filled
    automatically), when you type a character, current one gets replaced with
    new one.
    
    4. Click "SEND" button to send data to remote bluetooth device.
    First, app will try to connect to the remote device and send first row,
    than it wil disconnect and recconnect, sending another row. This prevents
    rows from being read as one row.
    
    This app was custom built for Arduino project, developed in Secondary
    school for technical professions Šiška, Ljublana, Slovenia.
    
    You may modify, customaze or use parts of this for your own needs.
    
    Updates:
    
    1. First upload, still not 100% finished!
    2. Aplication closes itself, if the device doesn't have Bluetooth adapter.

--- app/src/main/java/eonzombie/net/androiddisplaycontrol/MainActivity.java
+++ app/src/main/java/eonzombie/net/androiddisplaycontrol/MainActivity.java
@@ -4,11 +4,13 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -64,7 +66,7 @@ public class MainActivity extends AppCompatActivity
 
         if (ad == null)
         {
-
+          System.exit(1);
         }
         if (!ad.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
